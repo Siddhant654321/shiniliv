20cb8e234694a28af481d8af8bbc3b7f8d16ebac
Siddhant <technosys.sidd@gmail.com>
2023-07-24 18:10:59 -0400

    Fetch Products Complete

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,10 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -30,7 +34,12 @@ export const ProductsProvider = ({ children }) => {
   };
   const fetchProducts = async (url) => {
     const response = await axios.get(url);
-    console.log(response);
+    const products = response.data;
+    dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    try {
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
   };
   useEffect(() => {
     fetchProducts(url);
