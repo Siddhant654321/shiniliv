098486eed6eeda07fe8d814232e03b56c5f2673b
Robbie213 <sasnarinedeodat407@gmail.com>
2023-07-25 14:44:51 -0400

    Single Product - Base Return

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,15 @@
 export const formatPrice = (number) => {
-  const newNumber = new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   }).format(number / 100);
 };
 
-export const getUniqueValues = () => {};
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  if (type === "colors") {
+    unique = unique.flat();
+  }
+
+  return ["all", ...new Set(unique)];
+};
