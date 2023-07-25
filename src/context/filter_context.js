2ddc52f7778f8a736a3a3ea43861345d0e4426d5
Siddhant <technosys.sidd@gmail.com>
2023-07-25 18:26:47 -0400

    Sort Component - Controlled Input

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -16,6 +16,7 @@ const initialState = {
   filtered_products: [],
   all_products: [],
   grid_view: true,
+  sort: "price-lowest",
 };
 
 const FilterContext = React.createContext();
@@ -34,6 +35,12 @@ export const FilterProvider = ({ children }) => {
   const setListView = () => {
     dispatch({ type: SET_LISTVIEW });
   };
+  const updateSort = (e) => {
+    //for demonstration
+    // const name = e.target.name;
+    const value = e.target.value;
+    dispatch({ type: UPDATE_SORT, payload: value });
+  };
 
   return (
     <FilterContext.Provider
@@ -41,6 +48,7 @@ export const FilterProvider = ({ children }) => {
         ...state,
         setGridView,
         setListView,
+        updateSort,
       }}
     >
       {children}
