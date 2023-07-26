4e879eb3ef021a0eff6fa205ee5abdfe2eb7410b
Robbie213 <sasnarinedeodat407@gmail.com>
2023-07-26 17:11:27 -0400

    Stripe Checkout - Get Client Secret

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -47,11 +47,15 @@ const CheckoutForm = () => {
 
   const createPaymentIntent = async () => {
     try {
-      const data = await axios.post(
+      const { data } = await axios.post(
         "/.netlify/functions/create-payment-intent",
         JSON.stringify({ cart, total_amount, shipping_fee }),
       );
-    } catch (error) {}
+
+      setClientSecret(data.clientSecret);
+    } catch (error) {
+      // console.log(error.response);
+    }
   };
 
   useEffect(() => {
