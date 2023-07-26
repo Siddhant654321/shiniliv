8114fd756436eee32c87aec7f292f6e4250fb7ec
Siddhant <technosys.sidd@gmail.com>
2023-07-26 13:47:16 -0400

    AuthWrapper

--- src/App.js
+++ src/App.js
@@ -11,39 +11,46 @@ import {
   About,
   Products,
   PrivateRoute,
+  AuthWrapper,
 } from "./pages";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/cart">
-          <Cart />
-        </Route>
-        <Route exact path="/products">
-          <Products />
-        </Route>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+          <Route exact path="/products">
+            <Products />
+          </Route>
 
-        <Route exact path="/products/:id" children={<SingleProduct />}></Route>
+          <Route
+            exact
+            path="/products/:id"
+            children={<SingleProduct />}
+          ></Route>
 
-        <PrivateRoute exact path="/checkout">
-          <Checkout />
-        </PrivateRoute>
+          <PrivateRoute exact path="/checkout">
+            <Checkout />
+          </PrivateRoute>
 
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
