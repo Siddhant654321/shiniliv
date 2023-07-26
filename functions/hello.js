90824a03fb579d68cc1eced850c028351d1ca55d
Siddhant <technosys.sidd@gmail.com>
2023-07-26 14:53:41 -0400

    Stripe Checkout - Setup

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,12 @@
+//domain/.netlify/functions/hello
+
+const items = [
+  { id: 1, name: "john" },
+  { id: 2, name: "peter" },
+];
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: "Hello World",
+    body: "hello world",
   };
 };
