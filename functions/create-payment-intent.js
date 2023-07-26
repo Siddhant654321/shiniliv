ccfa7db7823f43c4b9052f3cb7632b02b1f3b259
Siddhant <technosys.sidd@gmail.com>
2023-07-26 16:47:52 -0400

    Netlify Function /create-payment-setup

---
+++ functions/create-payment-intent.js
@@ -0,0 +1,17 @@
+//doman/.netlify/functions/create-payment-intent
+
+exports.handler = async function (event, context) {
+  if (event.body) {
+    const { cart, total_amount, shipping_fee } = JSON.parse(event.body);
+    console.log(cart);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(cart),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: "create payment intent",
+  };
+};
